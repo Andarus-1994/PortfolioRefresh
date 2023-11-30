34a22925b9f645587934fa9bb5c2af16532cddeb
Andrei <andrei_king1@yahoo.com>
2023-11-30 18:41:41 +0200

    Added mouse effect for header

--- src/components/Topheader.js
+++ src/components/Topheader.js
@@ -1,9 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { faStar, faPlay, faBolt, faHashtag } from "@fortawesome/free-solid-svg-icons"
 import Lines from "./Lines"
 import { useEffect, useRef, useState } from "react"
+import { createRoot } from "react-dom/client"
 function TopHeader() {
   const [coords, setCoords] = useState({ x: 0, y: 0 })
+  const [prevCoords, setPrevCoords] = useState({ x: 0, y: 0 })
+
   const myName = useRef()
   const scrollTo = () => {
     const element = document.getElementById("portfolio").getBoundingClientRect().top + window.scrollY
@@ -29,6 +33,43 @@ function TopHeader() {
       spanElement.style.left = distanceFromCenterX / 200 + "px"
       spanElement.style.top = distanceFromCenterY / 200 + "px"
     }
+
+    const coords = { x: event.clientX, y: event.clientY }
+    const distance = calculateDistance(prevCoords, coords)
+    if (distance > 50) {
+      const star = document.createElement("span")
+      const root = createRoot(star)
+      root.render(<FontAwesomeIcon icon={getRandomIcon()} />)
+      star.classList.add("star")
+      const light = document.getElementsByClassName("TopHeader")
+      light[0].append(star)
+
+      if (star) {
+        const lightRect = light[0].getBoundingClientRect()
+        star.style.left = coords.x - lightRect.left + "px"
+        star.style.top = coords.y - lightRect.top + "px"
+        star.style.color = getRandomColor()
+        setTimeout(() => {
+          star.remove()
+        }, [750])
+      }
+      setPrevCoords(coords)
+    }
+  }
+  function calculateDistance(point1, point2) {
+    const dx = point1.x - point2.x
+    const dy = point1.y - point2.y
+    return Math.sqrt(dx * dx + dy * dy)
+  }
+  function getRandomColor() {
+    const colors = ["#00d9ff", "#ffffff", "#7da6ad"]
+    const randomIndex = Math.floor(Math.random() * colors.length)
+    return colors[randomIndex]
+  }
+  function getRandomIcon() {
+    const icons = [faPlay, faStar, faBolt, faHashtag]
+    const randomIndex = Math.floor(Math.random() * icons.length)
+    return icons[randomIndex]
   }
   return (
     <div
